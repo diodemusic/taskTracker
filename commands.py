7a5db9e6bef91eab8af3f5b91e488674065ee0dd
saves <111793440+diodemusic@users.noreply.github.com>
2024-10-30 19:43:44 +0000

    Create commands.py

---
+++ commands.py
@@ -0,0 +1,16 @@
+COMMANDS = [
+    "add",
+    "update",
+    "delete",
+    "mark-todo",
+    "mark-in-progress",
+    "mark-done",
+    "list",
+    "help",
+    "quit",
+]
+LIST_COMMANDS = [
+    "done",
+    "todo",
+    "in-progress",
+]
