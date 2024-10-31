63dc33814b57ff4531e44ac151044e4c961677c3
saves <111793440+diodemusic@users.noreply.github.com>
2024-10-31 00:11:44 +0000

    Update main.py
    Added docstrings

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from datetime import datetime
+#TODO: indexing can break when deleting tasks making some tasks share the same id
+
 import json
 import os
-import commands
 import sys
+from datetime import datetime
+
+import commands
 
 print(""" _____         _   _____             _           
 |_   _|___ ___| |_|_   _|___ ___ ___| |_ ___ ___ 
@@ -12,24 +15,47 @@ print(""" _____         _   _____             _
 
 PATH = "tasks.json"
 
+
 def check_if_json_exists() -> None:
+    """
+    Checks if a JSON file exists at the specified PATH.
+    If the file does not exist or is not readable, creates a new JSON file with an empty "tasks" array.
+
+    :return: None
+    """
     if os.path.isfile(PATH) and os.access(PATH, os.R_OK):
         pass
     else:
         with open(PATH, "w") as tasks_json:
             tasks_json.write(json.dumps({"tasks": []}))
 
+
 def load_json() -> dict:
+    """
+    Loads tasks from a JSON file.
+
+    :return: A dictionary containing the tasks loaded from the JSON file.
+    """
     with open("tasks.json", "r") as f:
         tasks = json.load(f)
     return tasks
 
+
 def save_json(tasks: dict) -> None:
+    """
+    :param tasks: A dictionary containing tasks to be saved in JSON format.
+    :return: None
+    """
     with open("tasks.json", "w") as f:
         # noinspection PyTypeChecker
         json.dump(tasks, f, indent=4)
 
+
 def create_task(task_description: str) -> None:
+    """
+    :param task_description: The description of the task to be created.
+    :return: None
+    """
     tasks = load_json()
 
     task_id = len(tasks["tasks"]) + 1
@@ -44,7 +70,13 @@ def create_task(task_description: str) -> None:
     save_json(tasks)
     print(f"Task {task_id} created.")
 
+
 def update_task(task_id: int, task_description: str) -> None:
+    """
+    :param task_id: The unique identifier for the task to be updated.
+    :param task_description: The new description for the task.
+    :return: None
+    """
     tasks = load_json()
 
     for task in tasks["tasks"]:
@@ -55,7 +87,12 @@ def update_task(task_id: int, task_description: str) -> None:
     save_json(tasks)
     print(f"Task {task_id} updated.")
 
+
 def delete_task(task_id: int) -> None:
+    """
+    :param task_id: The ID of the task to be deleted.
+    :return: None
+    """
     tasks = load_json()
 
     tasks["tasks"].pop(task_id - 1)
@@ -63,7 +100,13 @@ def delete_task(task_id: int) -> None:
     save_json(tasks)
     print(f"Task {task_id} deleted.")
 
+
 def mark_task(action: str, task_id: int) -> None:
+    """
+    :param action: The action to be taken on the task. It can be one of the following: 'mark-todo', 'mark-in-progress', or 'mark-done'.
+    :param task_id: The unique identifier of the task to be updated.
+    :return: None
+    """
     tasks = load_json()
 
     for task in tasks["tasks"]:
@@ -80,7 +123,12 @@ def mark_task(action: str, task_id: int) -> None:
     save_json(tasks)
     print(f"Task {task_id} status updated to {action[5:]}")
 
+
 def list_tasks(action: str) -> None:
+    """
+    :param action: The status of tasks to filter by. If empty, all tasks are listed.
+    :return: None
+    """
     tasks = load_json()["tasks"]
 
     print("")
@@ -95,6 +143,7 @@ def list_tasks(action: str) -> None:
                 for k, v in task.items():
                     print(f"{k}: {v}")
                 print("")
+
 
 if __name__ == "__main__":
     check_if_json_exists()
@@ -141,4 +190,5 @@ if __name__ == "__main__":
                 print(f"list {user_list_command}")
 
         elif command[0] == "quit":
+            print("Goodbye :)")
             sys.exit()
